278a7c5cc82b06dd25195897ca42d2e4b68cc1c6
Sghir Nouredine <nourabousoha@gmail.com>
2017-12-08 10:59:37 +0000

    Integrating the Database

--- mern_omment_box/server.js
+++ mern_omment_box/server.js
@@ -5,7 +5,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-
+var Comment = require('./model/comments');
 //and create our instances
 var app = express();
 var router = express.Router();
@@ -13,6 +13,8 @@ var router = express.Router();
 //set our port to either a predetermined port number if you have set 
 //it up, or 3001
 var port = process.env.API_PORT || 3001;
+//db config
+mongoose.connect('mongodb://bear-18:bear-18@ds135156.mlab.com:35156/comments');
 
 //now we should configure the API to use bodyParser and look for 
 //JSON data in the request body
